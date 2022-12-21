1bff7d761ab49e54d97b091d23be038a97aefeee
Citlali Melchor <62654605+CitlaliMelchor@users.noreply.github.com>
2022-12-22 00:22:01 +0100

    Add files via upload

--- MyCounter.py
+++ MyCounter.py
@@ -3,31 +3,19 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication
 import sys
 import csv
 import time
 
-class Window(QMainWindow):
+# class Window(QMainWindow):
+# class Ui_MainWindow(object):
+class Ui_MainWindow(QtWidgets.QMainWindow):
 
-    def __init__(self):
-        super().__init__()
-
-        # setting title
-        self.setWindowTitle("MyCounter ")
-
-        # setting geometry
-        self.setGeometry(2700, 100, 600, 200)
-
-        # calling method
-        self.UiComponents()
-
-        # showing all the widgets
-        self.show()
-
-    # method for widgets
-    def UiComponents(self):
-
-        # variables
+############################ UI construction ##############################
+    def setupUi(self, MainWindow):
+        # Variables
         self.week           = ''
         self.today_date     = ''
         self.start_hour     = ''
@@ -36,87 +24,164 @@ class Window(QMainWindow):
         self.task_B         = ''
         self.task_C         = ''
 
+        self.count_A = 0
+        self.count_B = 0
+        self.count_C = 0
+        self.flagA = False    
+        self.flagB = False    
+        self.flagC = False    
 
+
+        self.timer_A = QTimer()
+        self.timer_A.timeout.connect(self.countTimeA)
+        self.timer_A.start(100)
+
+        self.timer_B = QTimer()
+        self.timer_B.timeout.connect(self.countTimeB)
+        self.timer_B.start(100)
+
+        self.timer_C = QTimer()
+        self.timer_C.timeout.connect(self.countTimeC)
+        self.timer_C.start(100)
+
+
+        MainWindow.setObjectName("MyCounter")
+        MainWindow.resize(300, 200)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+
+        # 'Select your task' label 
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(40, 30, 211, 20))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        # Radio button for taskA
+        self.radioButton_taskA = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton_taskA.setGeometry(QtCore.QRect(50, 50, 95, 20))
+        self.radioButton_taskA.toggled.connect(self.taskAselected)
+         
+        # Radio button for taskB
+        self.radioButton_taskB = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton_taskB.setGeometry(QtCore.QRect(50, 80, 95, 20))
+        self.radioButton_taskB.toggled.connect(self.taskBselected)
+
+        # Radio button for taskC
+        self.radioButton_taskC = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton_taskC.setGeometry(QtCore.QRect(50, 110, 95, 20))
+        self.radioButton_taskC.toggled.connect(self.taskCselected)
+
+        # EDIT TASKS BUTTON
+        self.edit_tasks_button = QtWidgets.QPushButton(self.centralwidget)
+        self.edit_tasks_button.setGeometry(QtCore.QRect(50, 135, 50, 20))
+        self.edit_tasks_button.clicked.connect(self.edit_tasks)
+         
         # START DAY BUTTON
-        start_day_button = QPushButton("Start my day", self)
-        # setting geometry to the push button
-        start_day_button.setGeometry(100, 100, 150, 50)
-        # adding action to the button
-        start_day_button.clicked.connect(self.start_day)
-        # creating a label object
-        # self.label = QLabel("something",self)
-        # self.label.setGeometry(50, 50, 50, 50)
+        self.start_day_button = QtWidgets.QPushButton(self.centralwidget)
+        self.start_day_button.setGeometry(QtCore.QRect(150, 30, 100, 50))
+        self.start_day_button.clicked.connect(self.start_day)
+
+       # FINISH DAY BUTTON
+        self.finish_day_button = QtWidgets.QPushButton(self.centralwidget)
+        self.finish_day_button.setGeometry(QtCore.QRect(150, 100, 100, 50))
+        self.finish_day_button.clicked.connect(self.finish_day)
+
+       # WEEK REPORT BUTTON
+        self.week_report_button = QtWidgets.QPushButton(self.centralwidget)
+        self.week_report_button.setGeometry(QtCore.QRect(100, 170, 100, 20))
+        self.week_report_button.clicked.connect(self.week_report)
+
+ 
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+############################ Functions ##############################          
+    def countTimeA(self):
+        if self.flagA:
+            self.count_A+= 1
+
+    def countTimeB(self):
+        if self.flagB:
+            self.count_B+= 1
+
+    def countTimeC(self):
+        if self.flagC:
+            self.count_C+= 1   
 
 
-        # FINISH DAY BUTTON
-        finish_day_button = QPushButton("Finish my day", self)
-        # setting geometry to the push button
-        finish_day_button.setGeometry(250, 100, 150, 50)
-        # adding action to the button
-        finish_day_button.clicked.connect(self.finish_day)
-        # creating a label object
-        # self.label = QLabel("something",self)
-        # self.label.setGeometry(50, 50, 50, 50)
+    def taskAselected(self, selected):
+        if selected:
+            self.flagA = True
+            self.flagB = False
+            self.flagC = False
+            self.countTimeA()
 
+    def taskBselected(self, selected):
+        if selected:
+            self.flagB = True
+            self.flagA = False
+            self.flagC = False
+            self.countTimeB()
 
-    # method called by the start_day push button
+    def taskCselected(self, selected):
+        if selected:
+            self.flagC = True
+            self.flagA = False
+            self.flagB = False
+            self.countTimeC()
+         
+
     def start_day(self):
-
-        # getting current time
         current_time = QTime.currentTime()
-
-        # converting QTime object to string
         self.start_hour = current_time.toString('hh:mm:ss')
 
-        # save it in the start time variable
-        # self.label.setText(start_hour)
 
-    # method called by the finish_day push button
     def finish_day(self):
-
-        # getting current time
         current_time = QTime.currentTime()
         current_date = QDate.currentDate()
-        # converting QTime object to string
         self.finish_hour = current_time.toString('hh:mm:ss')
-
-        self.week = str(current_date.weekNumber)
-        self.today_date = str(current_date)
-        self.task_A = 'ddd'
-        self.task_B = 'sss'
-        self.task_C = 'kkk'
-
-        # save it in the start time variable
-        # self.label.setText(finish_hour)
-
-        # add a new row with the start and finish day into my csv data
-        newRow = "\n%s,%s,%s,%s,%s,%s,%s\n" % (self.week, self.today_date, self.start_hour, self.finish_hour, self.task_A, self.task_B, self.task_C)
-        with open("C:/Users/670273766/source/clarity_data.csv", "a") as f:
+        self.week = QDate.weekNumber(current_date)
+        self.week = self.week[0]
+        self.today_date = current_date.toString('dd-MM-yyyy')
+        self.task_A = time_A = str(self.count_A / 10)
+        self.task_B = time_B = str(self.count_B / 10)
+        self.task_C = time_C = str(self.count_C / 10)
+        newRow = "\n%d,%s,%s,%s,%s,%s,%s" % (self.week, self.today_date, self.start_hour, self.finish_hour, self.task_A, self.task_B, self.task_C)
+        with open("./clarity_data.csv", "a") as f:
             f.write(newRow)
+        # self.close()
 
+    def week_report(self):
+        print('report done')
 
-# create pyqt5 app
-App = QApplication(sys.argv)
-
-# create the instance of our Window
-window = Window()
-
-# showing all the widgets
-window.show()
-
-# start the app
-App.exit(App.exec_())
-
-
+    def edit_tasks(self):    
+        newtaskA, done1 = QtWidgets.QInputDialog.getText(
+             self, 'Task editor', 'Task A:')
+        newtaskB, done2 = QtWidgets.QInputDialog.getText(
+             self, 'Task editor', 'Task B:')
+        newtaskC, done3 = QtWidgets.QInputDialog.getText(
+             self, 'Task editor', 'Task C:')                     
 
 
 
+############################ Translate to UI #########################          
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
 
-# import time
-# import csv
+        MainWindow.setWindowTitle(_translate("MyCounter", "MyCounter"))
+        self.label.setText(_translate("MyCounter", "Select your task"))
+        self.radioButton_taskA.setText(_translate("MyCounter", "taskA"))
+        self.radioButton_taskB.setText(_translate("MyCounter", "taskB"))
+        self.radioButton_taskC.setText(_translate("MyCounter", "taskC"))
+        self.start_day_button.setText(_translate("MyCounter", "Start Day"))
+        self.finish_day_button.setText(_translate("MyCounter", "Finish Day"))
+        self.week_report_button.setText(_translate("MyCounter", "Weekly Report"))
+        self.edit_tasks_button.setText(_translate("MyCounter", "Edit tasks"))
 
-
-# today = (time.strftime("%d-%m-%Y"))
-# newRow = "\n%s,%s,%s,%s,%s,%s\n" % (today, yes, ok, war, leg, noag)
-# with open("clarity_data.csv", "a") as f:
-    # f.write(newRow)
+############################ Driver Code #########################
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+   
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
